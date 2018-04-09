ca8caad35ca2e66ded843d61047ff34bc9eed0a1
fgallet2016 <florentin.gallet@yahoo.fr>
2018-04-09 15:09:21 +0200

    Modif agence

--- app/src/main/java/com/example/fgallet2016/europcar/Activity/LocationVehiculeActivity.java
+++ app/src/main/java/com/example/fgallet2016/europcar/Activity/LocationVehiculeActivity.java
@@ -1,15 +1,43 @@
 package com.example.fgallet2016.europcar.Activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.fgallet2016.europcar.R;
 
+import org.w3c.dom.Text;
+
 public class LocationVehiculeActivity extends AppCompatActivity {
+
+    private TextView raison_social;
+    private TextView siret;
+    private TextView voie;
+    private TextView code_postal;
+    private TextView ville;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location_vehicule);
+
+        Intent intent = getIntent();
+
+        raison_social = findViewById(R.id.raison_social);
+        raison_social.setText(intent.getStringExtra("raison_social"));
+
+        siret = findViewById(R.id.siret);
+        siret.setText(intent.getStringExtra("siret"));
+
+        voie = findViewById(R.id.voie);
+        voie.setText(intent.getStringExtra("voie"));
+
+        code_postal = findViewById(R.id.code_postal);
+        code_postal.setText(intent.getStringExtra("code_postal"));
+
+        ville = findViewById(R.id.ville);
+        ville.setText(intent.getStringExtra("ville"));
+
     }
 }
--- app/src/main/java/com/example/fgallet2016/europcar/Activity/ModifAgenceActivity.java
+++ app/src/main/java/com/example/fgallet2016/europcar/Activity/ModifAgenceActivity.java
@@ -1,15 +1,50 @@
 package com.example.fgallet2016.europcar.Activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.example.fgallet2016.europcar.Fragment.ModifAgenceFragment;
 import com.example.fgallet2016.europcar.R;
 
 public class ModifAgenceActivity extends AppCompatActivity {
+
+
+    private EditText raison_social;
+    private EditText siret;
+    private EditText voie;
+    private EditText code_postal;
+    private EditText ville;
+    private Button bouton_modifier;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modif_agence);
+
+        this.raison_social = findViewById(R.id.raison_social);
+        this.siret = findViewById(R.id.siret);
+        this.voie = findViewById(R.id.voie);
+        this.code_postal = findViewById(R.id.code_postal);
+        this.ville = findViewById(R.id.ville);
+        this.bouton_modifier = findViewById(R.id.bouton_modifier);
+
+
+
+        bouton_modifier.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ModifAgenceActivity.this, LocationVehiculeActivity.class);
+                intent.putExtra("raison_social", raison_social.getText().toString());
+                intent.putExtra("siret", siret.getText().toString());
+                intent.putExtra("voie", voie.getText().toString());
+                intent.putExtra("code_postal", code_postal.getText().toString());
+                intent.putExtra("ville", ville.getText().toString());
+                startActivity(intent);
+            }
+        });
     }
 }
